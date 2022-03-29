700645fdab84ef92527bb23acb2fbe56aa0e90d3
moinsoft <mmoin.official@gmail.com>
2022-03-30 02:26:27 +0600

    find a single player by id

--- src/server.js
+++ src/server.js
@@ -17,11 +17,38 @@ app.use(express.json());
  * CRUD   -       Create Read Update Delete
  * GET    - /     - find all players
  * POST   - /     - create a new player and save into db
- * GET    - /:id  - find a single player
+ * GET    - /:id  - find a single player by id
  * PUT    - /:id  - update or create player
  * PATCH  - /:id  - update player
  * DELETE - /:id  - delete player from db
  */
+
+
+
+
+//  GET    - /:id  - find a single player by id
+
+app.get('/:id', async (req, res) => {
+  const id = req.params.id
+
+  const data = await fs.readFile(dbLocation);
+  const players = JSON.parse(data);
+
+  const player = players.find(item => item.id === id);
+
+  if (!player) {
+    return res.status(404).json({ message: 'Player Not Found' });
+  }
+
+
+  res.status(200).json(player);
+
+
+});
+
+
+
+
 
 
 
