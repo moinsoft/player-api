b3ff7e4e3a632373610eb3e156f47bdb18bc47f3
moinsoft <mmoin.official@gmail.com>
2022-03-30 02:02:40 +0600

    create a new player and save into db

--- src/server.js
+++ src/server.js
@@ -1,12 +1,54 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const shortid = require('shortid');
+const fs = require('fs/promises');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+
+/**
+ * Player Microservice
+ * CRUD   -       Create Read Update Delete
+ * GET    - /     - find all players
+ * POST   - /     - create a new player and save into db
+ * GET    - /:id  - find a single player
+ * PUT    - /:id  - update or create player
+ * PATCH  - /:id  - update player
+ * DELETE - /:id  - delete player from db
+ */
+
+
+
+//  POST   - /     - create a new player and save into db
+
+app.post('/', async (req, res) => {
+  const player = {
+    ...req.body,
+    id: shortid.generate()
+  };
+
+
+  const dbLocation = path.resolve('src', 'data.json')
+  const data = await fs.readFile(dbLocation);
+  const players = JSON.parse(data);
+  players.push(player)
+
+
+  await fs.writeFile(dbLocation, JSON.stringify(players));
+  res.status(201).json(player);
+
+});
+
+
+
+
+
+
 
 app.get('/health', (_req, res) => {
   res.status(200).json({ status: 'OK' });
