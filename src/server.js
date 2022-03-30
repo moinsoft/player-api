4a0a39fe6918087d6f16135e8abbc8ecf285bc2c
moinsoft <mmoin.official@gmail.com>
2022-03-30 10:33:48 +0600

    Update Player

--- src/server.js
+++ src/server.js
@@ -15,16 +15,43 @@ app.use(express.json());
 /**
  * Player Microservice
  * CRUD   -       Create Read Update Delete
- * GET    - /     - find all players
- * POST   - /     - create a new player and save into db
- * GET    - /:id  - find a single player by id
- * PUT    - /:id  - update or create player
- * PATCH  - /:id  - update player
+ * GET    - /     - find all players  (Done 2nd)
+ * POST   - /     - create a new player and save into db (Done 1st)
+ * GET    - /:id  - find a single player by id (Done 3rd)
+ * PUT    - /:id  - update all Data or create player
+ * PATCH  - /:id  - update player only name or id or rank (Done 4th)
  * DELETE - /:id  - delete player from db
  */
 
 
 
+
+
+
+//  PATCH  - /:id  - update player only name or id or rank
+
+app.patch('/:id', async (req, res) => {
+  const id = req.params.id
+
+  const data = await fs.readFile(dbLocation);
+  const players = JSON.parse(data);
+
+  const player = players.find(item => item.id === id);
+
+  if (!player) {
+    return res.status(404).json({ message: 'Player Not Found' });
+  }
+
+
+  player.name = req.body.name || player.name
+  player.country = req.body.country || player.country
+  player.rank = req.body.rank || player.rank
+
+  await fs.writeFile(dbLocation, JSON.stringify(players));
+  res.status(200).json(player);
+
+
+});
 
 //  GET    - /:id  - find a single player by id
 
